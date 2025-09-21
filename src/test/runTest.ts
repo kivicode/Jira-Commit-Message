178690bedbb16770ef5db7099952f007a2faf279
KiviCode <kivicode@yandex.com>
2025-09-21 02:52:33 +0200

    [exp] disable sandbox

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -19,7 +19,7 @@ async function main() {
       extensionDevelopmentPath,
       extensionTestsPath,
       version: process.env.VSCODE_VERSION || "1.95.0",
-      launchArgs: ["--disable-gpu", workspaceFolder],
+      launchArgs: ["--disable-gpu", "--no-sandbox", workspaceFolder],
     });
   } catch (err) {
     console.error("Failed to run tests", err);
