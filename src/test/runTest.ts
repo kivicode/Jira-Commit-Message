7929d2a09b050ee222a585d50c9fc1805a56b1fe
Frederik Claus <f.v.claus@googlemail.com>
2026-03-29 18:30:00 +0200

    Remove prefix when switching branches

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -19,7 +19,7 @@ async function main() {
       extensionDevelopmentPath,
       extensionTestsPath,
       version: process.env.VSCODE_VERSION || "1.95.0",
-      launchArgs: ["--disable-gpu", "--no-sandbox", workspaceFolder],
+      launchArgs: ["--disable-gpu", "--no-sandbox", "--profile-temp", workspaceFolder],
     });
   } catch (err) {
     console.error("Failed to run tests", err);
